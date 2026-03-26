eaa39cb689d0127d95015fa89adf8fe09ea5caf8
Prajwal Chavan <prajwalchavan@Prajwals-MacBook-Air.local>
2026-03-26 12:44:47 +0530

    after login auth changes

--- app/components/dashboard/Dashboard.tsx
+++ app/components/dashboard/Dashboard.tsx
@@ -1,11 +1,28 @@
 "use client";
 
-export default function Dashboard (){
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+
+export default function DashboardPage() {
+  const { data: session, status } = useSession();
+  const router = useRouter();
+
+  
+  useEffect(() => {
+    if (status === "unauthenticated") {
+      router.push("/login");
+    }
+  }, [status]);
+
+  if (status === "loading") {
+    return <div className="text-white">Loading...</div>;
+  }
+
   return (
-      <div>
-        <h2>test</h2>
-      </div>
-  )
+    <div className="text-white p-5">
+      <h1>Welcome to Dashboard</h1>
+      {/* <p>User: {session?.user?.email}</p> */}
+    </div>
+  );
 }
-
-
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 // import Image from "next/image";
 import Login from "./login/page";
 import Signup from "./signup/page";
+import Dashboard from "./dashboard/page";
 
 export default function Home() {
   return (
      <div>
       <Login/>
       <Signup/>
+      <Dashboard/>
     
      </div>
    
